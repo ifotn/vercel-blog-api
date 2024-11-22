6c14e87c835057736e51930a980817f4d5972124
ifotn <Rich.Freeman@GeorgianCollege.ca>
2024-11-22 15:02:14 -0500

    partition sid cookie

--- controllers/users.js
+++ controllers/users.js
@@ -41,7 +41,8 @@ const clearTokenCookie = (res) => {
     });
     res.cookie('connect.sid', '', { 
         httpOnly: true,
-        expires: new Date(0)
+        expires: new Date(0),
+        partitioned: true
     });
     //console.log(res.cookie('authToken'));
 };
